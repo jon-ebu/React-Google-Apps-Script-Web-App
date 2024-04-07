ca3eff9cdb6a4239a819714444423af426ac48cc
jebuengHMC <ebueng23@gmail.com>
2024-04-07 13:59:32 -0700

    Creating basic HelloWorld component

---
+++ src/client/main/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+    return (
+        <div>
+            <p>Hello world!</p>
+        </div>
+    );
+}
+
+export default HelloWorld;
